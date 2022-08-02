b379d852cdb0c302d254732e7a0cb04fbf219847
Jordan <43437534+Jaardy@users.noreply.github.com>
2022-08-02 13:10:30 +0100

    w5-d1: live-code on classes

---
+++ Workshops/Part-2/week-5/day-1/classes.js
@@ -0,0 +1,40 @@
+class Book {
+  constructor(title, ISBN, author) {
+    this.title = title;
+    this.ISBN = ISBN;
+    this.author = author;
+    this.isAvailable = true;
+    this.currentOwner = null;
+  }
+}
+
+class User {
+  constructor(username, password) {
+    this.username = username;
+    this.password = password;
+    this.books = [];
+  }
+  updateUsername(newUserName, password) {
+    if (password === this.password) {
+      this.username = newUserName;
+      console.log('Username successfuly updated!');
+    } else {
+      console.log('The password is incorrect.');
+    }
+  }
+  checkoutBook(book) {
+    if (book.isAvailable) {
+      this.books.push(book);
+      book.currentOwner = this.username;
+      book.isAvailable = false;
+    }
+  }
+  returnBook(book) {
+    if (this.books.includes(book)) {
+      let index = this.books.indexOf(book);
+      this.books.splice(index, 1);
+      book.currentOwner = null;
+      book.isAvailable = true;
+    }
+  }
+}
