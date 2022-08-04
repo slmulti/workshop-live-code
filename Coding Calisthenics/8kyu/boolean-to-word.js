bea2341103a4035dfd0732ad3d60fdd83e10b9e3
Simon Love <simon-love@hotmail.co.uk>
2022-08-04 15:15:11 +0100

    04/08 2 challenges complete

--- Coding Calisthenics/8kyu/boolean-to-word.js
+++ Coding Calisthenics/8kyu/boolean-to-word.js
@@ -1,3 +1,17 @@
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
-let testBool = true;
+// let testBool = true;
+
+function boolToWord(bool){
+    if(bool==true){
+        return "yes"
+    } else return "no"
+}
+
+console.log(boolToWord(true))
+console.log(boolToWord(false))
+
+const boolToString = (b) => (b ? 'Yes' : 'No');
+
+console.log("javascript ternary operator way: " + boolToString(true))
+console.log("javascript ternary operator way: " + boolToString(false))
--- Coding Calisthenics/8kyu/well-of-ideas.js
+++ Coding Calisthenics/8kyu/well-of-ideas.js
@@ -3,4 +3,75 @@
 
 //eg   ['good', 'good', 'good', 'bad', good]  would return the string 'I smell a series!' as there are more than 2 'good' values
 
-let testArray = ['good', 'good', 'good', 'bad', good];
+let testArray1 = ['good', 'good', 'good', 'bad', 'good'];
+let testArray2 = ['good', 'good', 'bad', 'bad', 'bad'];
+let testArray3 = ['bad', 'bad', 'bad', 'bad', 'bad'];
+
+  
+
+function goodIdeas (arr) {
+
+    let count = 0
+
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i]==='good'){
+            count++
+        }
+    }
+    if(count===0) return "Fail!"
+    if(count>2) return "I Smell a Series!!!"
+    return "Publish!"
+}
+
+console.log(goodIdeas(testArray1))
+console.log(goodIdeas(testArray2))
+console.log(goodIdeas(testArray3))
+
+//===================================================================================
+
+// function goodIdeas (arr) {
+
+//     let count = 0
+
+//     for(let i = 0; i<arr.length; i++){
+//         if(arr[i]==='good'){
+//             count++
+//         }
+
+//     }
+//     if(count===0){
+//         return "Fail!"
+//     }else if(count>2){
+//         return "I Smell a Series!!!"
+//     } else return "Publish!"
+// }
+
+// console.log(goodIdeas(testArray1))
+// console.log(goodIdeas(testArray2))
+// console.log(goodIdeas(testArray3))
+
+//===================================================================================
+
+// let goodCount = 0
+// let badCount = 0
+
+// function checkArray(arr){
+//     for (let i = 0; i < arr.length; i++){
+//         if (arr[i] == "good"){
+//             goodCount++
+//         } else 
+//             badCount++
+//     }
+//     if (goodCount === 0) return "Fail"
+//     if (goodCount === 1 || goodCount === 2){
+//         return "Publish"
+//     } else if (goodCount > 2){
+//         return "I smell a series"
+//     }
+// }
+
+// // console.log(checkArray(["good", "good", "good", "good", "bad", "good"]))
+// // console.log(checkArray(["good", "good", "bad", "bad", "bad", "bad"]))
+// console.log(checkArray(["bad", "bad", "bad", "bad", "bad", "bad"]))
+// console.log(goodCount)
+// console.log(badCount)
